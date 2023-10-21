c385561425646b17d5fd77f972a89c4b4dc2919a
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-10-21 21:36:58 +0800

    remove util.cmd

--- scripts/install_amd.bat
+++ scripts/install_amd.bat
@@ -4,9 +4,18 @@ call env.cmd
 
 echo Download AMD Driver ...
 REM TODO: cannot download
-call util.cmd curl %AMD_DRIVER_URL% %INSTALL_ROOT%\%AMD_DRIVER_FILE%
+call:curl %AMD_DRIVER_URL% %INSTALL_ROOT%\%AMD_DRIVER_FILE%
 
 REM echo Installing AMD Driver ...
 REM REM TODO: do we need to do this?
+
+goto :exit
+
+:curl
+%_CURL% -L %~1 --output %~2
+exit /b
+
+:exit
+pause
 
 endlocal
